91cf9360349b382396fe5abd572876a3e15f88e0
thzero <thzero@users.noreply.github.com>
2023-03-29 07:37:53 -0500

    updates

--- features.js
+++ features.js
@@ -3,8 +3,23 @@ import AppSharedConstants from '@/utility/constants';
 import BaseFeaturesService from '@thzero/library_client/service/features';
 
 class FeatureService extends BaseFeaturesService {
+	constructor() {
+		super();
+
+		this._features = null;
+	}
+
 	features() {
-		return AppSharedConstants.Features;
+		if (this._features)
+			return this._features;
+
+		const features = AppSharedConstants.Features;
+		this._featuresOverride(features);
+		this._features = features;
+		return features;
+	}
+
+	_featuresOverride(features) {
 	}
 }
 
