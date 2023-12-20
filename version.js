a007861a0db047971cb17fe67d9c19a6f27e9112
thzero <thzero@users.noreply.github.com>
2023-12-20 12:30:09 -0600

    updates for vite5

--- version.js
+++ version.js
@@ -1,7 +1,8 @@
 import VersionService from '@thzero/library_client/service/version';
 
 // eslint-disable-next-line
-const modules = import.meta.globEager('../../package.json');
+// const modules = import.meta.globEager('../../package.json');
+const modules = import.meta.glob('../../package.json' ,{ eager: true });
 // eslint-disable-next-line camelcase
 const { version_major, version_minor, version_patch, version_date, copyright, author, author_url } = modules['../../package.json'];
 
