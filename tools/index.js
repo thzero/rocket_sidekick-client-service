2515601fc5e106e41b4b02dbef1a3872c4b901d7
Chris Schletter <development@thzero.com>
2024-11-12 13:11:20 -0600

    updates

--- tools/index.js
+++ tools/index.js
@@ -3,6 +3,15 @@ import AppCommonConstants from 'rocket_sidekick_common/constants';
 import BaseService from '@thzero/library_client/service/index';
 
 class ToolsService extends BaseService {
+	_checkField(correlationId, input, error, errors) {
+		if (input === -1) {
+			valid = false;
+			errors.push(error);
+			return false;
+		}
+		return true;
+	}
+
 	_measurementUnitFromId(correlationId, measurementUnitsId, measurementUnitType, measurementUnitId) {
 		const units = AppCommonConstants.MeasurementUnits[measurementUnitsId];
 		if (!units)
