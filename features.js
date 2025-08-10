bbf7af678783ea87473ef6bc4b690e65298a1766
Corvette Rox <development@thzero.com>
2025-08-10 09:23:12 -0500

    updates for gallery

--- features.js
+++ features.js
@@ -14,8 +14,11 @@ class FeatureService extends BaseFeaturesService {
 			return this._features;
 
 		const features = AppSharedConstants.Features;
+		console.log(features, 'features');
 		this._featuresOverride(features);
+		console.log(features, 'features.after.override');
 		this._features = features;
+		console.log(this._features, 'features.final');
 		return features;
 	}
 
